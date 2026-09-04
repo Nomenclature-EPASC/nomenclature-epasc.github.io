8bb89c20d8b3608b7c0ba0fe47b3a539c96d7ca2
Nomenclature-EPASC <maximemairesse13@gmail.com>
2026-09-04 22:43:33 +0200

    Améliore la mise à jour de la PWA

--- sw.js
+++ sw.js
@@ -1,22 +1,78 @@
-const VERSION = "v1";
+const VERSION = "v2";
+const CACHE_NAME = "nomenclature-" + VERSION;
 
-self.addEventListener("install", function() {
+self.addEventListener("install", function(event) {
+
     self.skipWaiting();
+
 });
 
 self.addEventListener("activate", function(event) {
+
     event.waitUntil(
-        self.clients.claim()
+
+        caches.keys().then(function(cacheNames) {
+
+            return Promise.all(
+
+                cacheNames
+                    .filter(function(cacheName) {
+                        return cacheName.startsWith("nomenclature-");
+                    })
+                    .filter(function(cacheName) {
+                        return cacheName !== CACHE_NAME;
+                    })
+                    .map(function(cacheName) {
+                        return caches.delete(cacheName);
+                    })
+
+            );
+
+        }).then(function() {
+
+            return self.clients.claim();
+
+        })
+
     );
+
 });
 
 self.addEventListener("fetch", function(event) {
 
     event.respondWith(
+
         fetch(event.request)
-            .catch(function() {
-                return caches.match(event.request);
+            .then(function(response) {
+
+                if (
+                    response.ok &&
+                    event.request.method === "GET"
+                ) {
+
+                    const responseClone =
+                        response.clone();
+
+                    caches.open(CACHE_NAME).then(function(cache) {
+
+                        cache.put(
+                            event.request,
+                            responseClone
+                        );
+
+                    });
+
+                }
+
+                return response;
+
             })
+            .catch(function() {
+
+                return caches.match(event.request);
+
+            })
+
     );
 
 });
